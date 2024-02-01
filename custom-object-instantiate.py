97830c01d45354e9488bac326bf353b75a6bf926
ecodes-w <121530929+ecodes-w@users.noreply.github.com>
2024-01-31 19:10:19 -0600

    Create custom-object-instantiate.py

---
+++ custom-object-instantiate.py
@@ -0,0 +1,23 @@
+# Define class MyFirstClass
+class MyFirstClass():
+
+    # Define string variable called index
+    print("Who wrote this?")
+    index = "Author-Book"
+    
+    # Define function hand_list()
+    def hand_list(self, philosopher, book):
+        print(MyFirstClass.index)
+        
+    
+        # variable + “ wrote the book: ” + variable
+        print(philosopher + " wrote the book: " + book)
+        
+
+# Call function handlist()
+whodunnit = MyFirstClass()
+whodunnit.hand_list("Sun Tzu", "The Art of War")
+
+
+
+
